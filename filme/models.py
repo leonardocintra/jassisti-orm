39095965c1633d5fed2e90dbb4e8c92e79cc8620
Leonardo Cintra <leonardo.ncintra@outlook.com>
2018-12-18 19:33:52 -0200

    create filme model

--- filme/models.py
+++ filme/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Filme(models.Model):
+    titulo = models.CharField('Título', max_length=80)
+    the_movie_db_id = models.IntegerField('ID TMD')
+    data_lancamento = models.DateField('Data lançamento')
+    sinopse = models.TextField('Sinopse')
+    imagem_poster = models.CharField('URL Poster', max_length=300)
+
+    def __str__(self):
+        return self.titulo
+
+    class Meta:
+        db_table = 'filme'
+        verbose_name = 'Filme'
+        verbose_name_plural = 'Filmes'
+        ordering = ['-data_lancamento']
